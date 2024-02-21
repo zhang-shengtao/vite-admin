918652a213f007f79b44d5d9d6b3104b43b6464f
zst <2356001919@qq.com>
2024-02-21 22:47:42 +0800

    考虑子线程如何切片

--- src/utils/method.js
+++ src/utils/method.js
@@ -1,5 +1,4 @@
 import Clipboard from "clipboard";
-import SparkMD5 from "spark-md5";
 import myWorkerjs from "./sliceFile.js?raw";
 
 /**
@@ -130,40 +129,23 @@ export function debounceRef(value = "", t = 1000) {
  */
 export function sliceFile(file) {
   console.time("分片耗时");
-  // if(!Array.isArray(file)) file = [file]
-  const spark = new SparkMD5.ArrayBuffer();
   const fileList = [];
   const size = 1024 * 1024 * 5; // 每片大小2M
   const sliceFileTotal = Math.ceil(file.size / size); // 总共要切多少片
   const cupNum = navigator.hardwareConcurrency; // cup核心数
 
-  const myWorker = new Worker(window.URL.createObjectURL(new Blob([myWorkerjs], { type: "application/javascript" })));
-  myWorker.postMessage("主线程得消息");
-  myWorker.onmessage = function (e) {
-    console.log(e.data);
-    myWorker.terminate();
-  };
+  // for (let index = 0; index < cupNum; index++) {
+  //   const myWorker = new Worker(window.URL.createObjectURL(new Blob([myWorkerjs], { type: "application/javascript" })));
+  //   myWorker.postMessage({
+  //     file,
 
-  let start = 0;
-  let end = 0;
-  for (let i = 0; i < sliceFileTotal; i++) {
-    start = i * size;
-    end = (i + 1) * size >= file.size ? file.size : (i + 1) * size;
-    const fileReader = new FileReader();
-    const blob = file.slice(start, end, file.type);
-    fileReader.onload = (e) => {
-      spark.append(e.target.result);
-      fileList.push({
-        blob,
-        start,
-        end,
-        index: i,
-        hash: spark.end()
-      });
-      if (i + 1 >= sliceFileTotal) console.log(fileList);
-    };
-    fileReader.readAsArrayBuffer(blob);
-  }
+  //   });
+  //   myWorker.onmessage = function (e) {
+  //     console.log(e.data);
+  //     myWorker.terminate();
+  //   };
+  // }
+
   // console.log(file);
   console.timeEnd("分片耗时");
 }
--- src/utils/sliceFile.js
+++ src/utils/sliceFile.js
@@ -1,5 +1,31 @@
+import SparkMD5 from "spark-md5";
+const spark = new SparkMD5.ArrayBuffer();
+
 // 主线程发送消息过来
 self.onmessage = function (e) {
   console.log(e.data);
   self.postMessage("向主线程发送消息");
 };
+
+function fn() {
+  let start = 0;
+  let end = 0;
+  for (let i = 0; i < sliceFileTotal; i++) {
+    start = i * size;
+    end = (i + 1) * size >= file.size ? file.size : (i + 1) * size;
+    const fileReader = new FileReader();
+    const blob = file.slice(start, end, file.type);
+    fileReader.onload = (e) => {
+      spark.append(e.target.result);
+      fileList.push({
+        blob,
+        start,
+        end,
+        index: i,
+        hash: spark.end()
+      });
+      if (i + 1 >= sliceFileTotal) console.log(fileList);
+    };
+    fileReader.readAsArrayBuffer(blob);
+  }
+}
